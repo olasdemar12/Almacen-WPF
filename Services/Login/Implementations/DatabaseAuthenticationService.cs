3e75a8a4346c00b5384aaf7f6a8cf884634fbd86
Jesus <charadremur01@gmail.com>
2026-03-05 22:39:20 -0600

    Dirzo: Servicios y clases

---
+++ Services/Login/Implementations/DatabaseAuthenticationService.cs
@@ -0,0 +1,23 @@
+﻿using Almacen_Sistema.MVVM.Models.Login;
+using Almacen_Sistema.Services.Login.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Almacen_Sistema.Services.Login.Implementations
+{
+    public class DatabaseAuthenticationService : AIAuthenticationService
+    {
+        public UserAccess AuthenticateUser(string username, string password)
+        {
+            var user = UserAccess.Users
+                        .FirstOrDefault(u =>
+                            u.NameUser == username &&
+                            u.PasswordUser == password);
+
+            return user; // Retorna objeto si existe, null si no
+        }
+    }
+}

8b3a939be2b6e90368113720a87a7cdbf003da90
olasdemar12 <12162005j@gmail.com>
2026-03-10 11:54:15 -0600

    Jorge: Desarrollo de panel reutilizable

---
+++ Resources/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Converters
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool state && state)
+                return Visibility.Visible;
+
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Visibility visibility)
+                return visibility == Visibility.Visible;
+
+            return false;
+        }
+    }
+}
---
+++ UI/Controls/Panel/PanelControl.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Markup;
+
+namespace StockMasterControls
+{
+    /// <summary>
+    /// Lógica de interacción para PanelControl.xaml
+    /// </summary>
+    [ContentProperty(nameof(PanelContent))]
+    public partial class PanelControl : UserControl
+    {
+        public PanelControl()
+        {
+            InitializeComponent();
+        }
+
+        //Propiedad para el Titulo del Panel
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register(
+                nameof(Title),
+                typeof(string),
+                typeof(PanelControl),
+                new PropertyMetadata("Panel Title"));
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        //Estado para mostrar boton de Regresar
+        public static readonly DependencyProperty ShowBackButtonProperty =
+            DependencyProperty.Register(
+                nameof(ShowBackButton),
+                typeof(bool),
+                typeof(PanelControl),
+                new PropertyMetadata(true));
+
+        public bool ShowBackButton
+        {
+            get { return (bool)GetValue(ShowBackButtonProperty); }
+            set { SetValue(ShowBackButtonProperty, value); }
+        }
+
+        //Estado paara mostrar el Boton de Cerrar panel
+        public static readonly DependencyProperty ShowCloseButtonProperty =
+            DependencyProperty.Register(
+                nameof(ShowCloseButton),
+                typeof(bool),
+                typeof(PanelControl),
+                new PropertyMetadata(true));
+
+        public bool ShowCloseButton
+        {
+            get { return (bool)GetValue(ShowCloseButtonProperty); }
+            set { SetValue(ShowCloseButtonProperty, value); }
+        }
+
+        //Contenido a mostrar en el Panel
+        public static readonly DependencyProperty PanelContentProperty =
+            DependencyProperty.Register(
+                nameof(PanelContent),
+                typeof(object),
+                typeof(PanelControl),
+                new PropertyMetadata(null));
+
+        public object PanelContent
+        {
+            get { return GetValue(PanelContentProperty); }
+            set { SetValue(PanelContentProperty, value); }
+        }
+
+    }
+}
